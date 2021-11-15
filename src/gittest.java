c5496b3aee2f7f29d2bd9d14b0266c03b45648f3
Alex-mp4 <alexander.heino@elev.ga.ntig.se>
2021-11-15 11:16:50 +0100

    First Övning start

--- src/gittest.java
+++ src/gittest.java
@@ -2,7 +2,21 @@ import javax.swing.*;
 
 public class gittest {
     public static void main(String[] args) {
-        String s;
-        s = JOptionPane.showInputDialog("Wow");
+        String nr;
+        String ut = null;
+        nr = JOptionPane.showInputDialog("Hur många tal?:");
+        int nr1 = Integer.parseInt(nr);
+        int i = 0;
+        int o = 0;
+
+                while(nr1 > i) {
+                    ut = JOptionPane.showInputDialog("Välj ett heltal:");
+                    int ut1 = Integer.parseInt(ut);
+                    o += ut1;
+                    i++;
+                }
+                int ut2 = o;
+                ut2 = ut2/nr1;
+                System.out.println(ut2);
     }
 }
